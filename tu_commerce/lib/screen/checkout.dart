cee43adb09d7549f724ba34f34bb2a075e5538ec
peekice <peekice123@gmail.com>
2024-04-25 16:26:59 +0700

    fix checkout

--- tu_commerce/lib/screen/checkout.dart
+++ tu_commerce/lib/screen/checkout.dart
@@ -92,7 +92,7 @@ class _CheckOutState extends State<CheckOut> {
 
                   Navigator.pushReplacement(
                     context, 
-                    MaterialPageRoute(builder: (context) => NavigationCustomer(email: widget.username['email'],temp: 8,order: orderMap,))
+                    MaterialPageRoute(builder: (context) => NavigationCustomer(email: widget.username['email'],temp: 8,order: orderMap,chatName: widget.username['username'],))
                   );
                 }else {
                     ScaffoldMessenger.of(context).showSnackBar(
