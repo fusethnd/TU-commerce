c6bd3c62979f2e6f5542aca82b3ef44da8630dc5
6410615030 <6410615030@student.tu.ac.th>
2024-04-28 20:53:07 +0700

    noti seller

--- tu_commerce/lib/screen/home.dart
+++ tu_commerce/lib/screen/home.dart
@@ -9,36 +9,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Register/Login'),),
+      appBar: AppBar(
+        title: Image.asset('assets/images/Logo.png'),
+        backgroundColor: const Color.fromRGBO(54, 91, 109, 1.0),
+        toolbarHeight: 700,
+        automaticallyImplyLeading: false,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(20),
+        )),
+      ),
       body: Column(
-        children: 
-          [
-            SizedBox(
-              child: ElevatedButton.icon(onPressed: ()
-              {
-                Navigator.push(
-                  context,MaterialPageRoute(builder: (context){
-                    return const RegisterScreen();
-                  })
-                );
-              }, 
-              icon: const Icon(Icons.add), 
-              label: const Text("Register")),
+        children: [
+          // SizedBox(
+          //   child: ElevatedButton.icon(onPressed: ()
+          //   {
+          //     Navigator.push(
+          //       context,MaterialPageRoute(builder: (context){
+          //         return const RegisterScreen();
+          //       })
+          //     );
+          //   },
+          //   icon: const Icon(Icons.add),
+          //   label: const Text("Register")),
+          // ),
+          Text(
+            "Welcome to TU-Commerce",
+            style: TextStyle(
+              fontSize: 16, // Adjust the font size as needed
+              color: Colors.white, // Set the text color
             ),
-            SizedBox(
-              child: ElevatedButton.icon(onPressed: ()
-              {
-                Navigator.push(
-                  context,MaterialPageRoute(builder: (context){
+          ),
+          Text(
+            "Shop for everything",
+            style: TextStyle(
+              fontSize: 12, // Adjust the font size as needed
+              color: Colors.white, // Set the text color
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            child: ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
                     return Login();
-                  })
-                );
-              }, 
-              icon: const Icon(Icons.login), 
-              label: const Text("Login")),
-            )
-          ],
-        ),
+                  }));
+                },
+                icon: const Icon(Icons.login),
+                label: const Text("Get Started")),
+          )
+        ],
+      ),
     );
   }
 }
--- tu_commerce/lib/screen/noticeSeller.dart
+++ tu_commerce/lib/screen/noticeSeller.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:tu_commerce/main.dart';
 
 class NoticeSeller extends StatefulWidget {
@@ -23,73 +24,120 @@ class _NoticeSellerState extends State<NoticeSeller> {
 
   Future<void> _initialState() async {
     Map<String, dynamic>? tempMap = (await FirebaseFirestore.instance
-        .collection('Notice')
-        .doc("seller"+widget.username['username'])
-        .get())
+            .collection('Notice')
+            .doc("seller" + widget.username['username'])
+            .get())
         .data();
     if (tempMap == null) {
-      tempMap = {
-        "length":0,
-        "noticeList":[]
-      };
+      tempMap = {"length": 0, "noticeList": []};
     }
-    if (mounted){
+    if (mounted) {
       setState(() {
         allNotice = tempMap;
       });
     }
-
   }
 
   @override
   Widget build(BuildContext context) {
     // print(allNotice!['noticeList']);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notifications'),
-      ),
+        appBar: AppBar(
+          title: const Text('Notifications'),
+        ),
+        body: allNotice == null || allNotice!['noticeList'] == null
+            ? const Center(child: CircularProgressIndicator())
+            : Column(
+                children: [
+                  Expanded(
+                    // Wrap ListView.builder with Expanded
+                    child: ListView.builder(
+                        padding: const EdgeInsets.all(10),
+                        itemCount: allNotice!['noticeList'].length,
+                        itemBuilder: (context, index) {
+                          Map<String, dynamic> notice =
+                              allNotice!['noticeList'][index];
+                          String status = notice['status'];
 
-      body: allNotice == null || allNotice!['noticeList'] == null
-      ? const Center(child: CircularProgressIndicator()):
-      Column(
-        children: [
-            Expanded( // Wrap ListView.builder with Expanded
-              child: ListView.builder(
-                padding: const EdgeInsets.all(10),
-                itemCount: allNotice!['noticeList'].length,
-                itemBuilder: (context, index) {
-                  Map<String, dynamic> notice = allNotice!['noticeList'][index];
-                  String status = notice['status'];
-                  return Card(
-                    shape: const RoundedRectangleBorder(),
-                    child: ListTile(
-                      title: Text('Status: $status'),
-                    ),
-                  );
-                }
-              ),
-            ),
+                          DateTime timestamp = notice!['time'].toDate();
+                          String formattedDate =
+                              DateFormat.yMMMd().format(timestamp);
 
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: () async {
-                if (allNotice!['noticeList'].isEmpty == false){
-            
-                  await FirebaseFirestore.instance.collection('Notice').doc("seller${widget.username['username']}").delete();
-                }
-                // setState(() {
-                //   allNotice = null;
-                // });
-                _initialState();
-              },
-              child: const Text('Clear')
-            ),
-          )
-
-
-        ],
-      )
-    );
+                          return Card(
+                            shape: const RoundedRectangleBorder(),
+                            // child: ListTile(
+                            //   title: Text('Status: $status'),
+                            // ),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 100,
+                                  height: 100,
+                                  child: Image.network(
+                                    notice['product']['link'],
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    color: Colors.grey[300],
+                                    height: 100,
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                (status),
+                                                style: const TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 17),
+                                              ),
+                                              Text("Product: " +
+                                                  notice['product']
+                                                      ['prodName']),
+                                              Text("@" +
+                                                  notice['product']['seller']
+                                                      ['username']),
+                                            ],
+                                          ),
+                                        ),
+                                        Text(formattedDate),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          if (allNotice!['noticeList'].isEmpty == false) {
+                            await FirebaseFirestore.instance
+                                .collection('Notice')
+                                .doc("seller${widget.username['username']}")
+                                .delete();
+                          }
+                          // setState(() {
+                          //   allNotice = null;
+                          // });
+                          _initialState();
+                        },
+                        child: const Text('Clear')),
+                  )
+                ],
+              ));
   }
 }
